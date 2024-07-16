6a49c70034a00ac228d170be9245f33679b6bdf2
muameh <balmehmet111@gmail.com>
2024-07-16 16:12:37 +0300

    modifiers added

--- app/src/main/java/com/mehmetbaloglu/jetpackcomposelessonsbeginnerlevel/MainActivity.kt
+++ app/src/main/java/com/mehmetbaloglu/jetpackcomposelessonsbeginnerlevel/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -16,8 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mehmetbaloglu.jetpackcomposelessonsbeginnerlevel.ui.theme.JetpackComposeLessonsBeginnerLevelTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,27 +36,38 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SampleText() {
 
-    var number = remember { mutableStateOf(0) }
+    var number = remember {
+        mutableStateOf(0)
+    }
 
-    Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-        
-        Text(text = number.value.toString(),Modifier.padding(10.dp))
-        
-        Text(text = "Add", modifier = Modifier.clickable {
-                number.value++
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-        }, color = Color.Red)
+        Text(
+            text = number.value.toString(), fontSize = 25.sp, color = Color.Blue
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+
+            OutlinedButton(onClick = { number.value-- }, modifier = Modifier.fillMaxWidth().weight(50f)) {
+                Text(text = "çıkar")
+            }
+            
+            Button(onClick = { number.value++ }, modifier = Modifier.fillMaxWidth().weight(50f)) {
+                Text(text = "artır")
+            }
+
+        }
+
 
     }
 
 
-
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SampleText()
-}
+
