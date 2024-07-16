2fdc61a5a47c7e48ca20f5058f0fd8519cec6f41
muameh <balmehmet111@gmail.com>
2024-07-16 21:03:48 +0300

    columns and rows are working

--- app/src/main/java/com/mehmetbaloglu/jetpackcomposelessonsbeginnerlevel/MainActivity.kt
+++ app/src/main/java/com/mehmetbaloglu/jetpackcomposelessonsbeginnerlevel/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mehmetbaloglu.jetpackcomposelessonsbeginnerlevel.ui.theme.JetpackComposeLessonsBeginnerLevelTheme
 
@@ -41,28 +44,64 @@ fun SampleText() {
     }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Text(
-            text = number.value.toString(), fontSize = 25.sp, color = Color.Blue
+            text = number.value.toString(), fontSize = 25.sp, color = Color.Black
         )
 
         Row(
             modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
         ) {
 
-            OutlinedButton(onClick = { number.value-- }, modifier = Modifier.fillMaxWidth().weight(50f)) {
+            OutlinedButton(
+                onClick = { number.value-- },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(33f)
+                    .padding(3.dp)
+            ) {
                 Text(text = "çıkar")
             }
-            
-            Button(onClick = { number.value++ }, modifier = Modifier.fillMaxWidth().weight(50f)) {
+
+            Button(
+                onClick = { number.value++ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(33f)
+                    .padding(3.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red,
+                    contentColor = Color.White
+                )
+            )
+
+            {
+                Text(text = "artır")
+            }
+            Button(
+                onClick = { number.value++ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(33f)
+                    .padding(3.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red,
+                    contentColor = Color.White
+                )
+            )
+
+            {
                 Text(text = "artır")
             }
 
         }
+
 
 
     }
